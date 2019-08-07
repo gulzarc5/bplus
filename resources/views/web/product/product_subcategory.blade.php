d56a3f51a114f3f6594eb288a1c54f78c2a65cfa
chimdevi <chinmayeedevi99@gmail.com>
2019-08-07 13:41:25 +0530

    second category design change

--- resources/views/web/product/product_subcategory.blade.php
+++ resources/views/web/product/product_subcategory.blade.php
@@ -21,11 +21,8 @@
 <div class="gap" style="margin-top: -20px;">
 </div>
 <div class="container">
-  <h4 >kitchen, Cookware & Serveware
-  </h4>
-  <hr>
   <div class="row row-sm-gap" data-gutter="15">
-    <div class="col-md-2">
+    <div class="col-md-3">
       <a class="banner-category" href="{{url('product_saller')}}">
         <img class="banner-category-img" src="{{('src/img/kitchen_icon/2.png')}}" alt="Image Alternative text" title="Image Title" />
         <h5 class="banner-category-title">Pans
@@ -34,7 +31,7 @@
         </p>
       </a>
     </div>
-    <div class="col-md-2">
+    <div class="col-md-3">
       <a class="banner-category" href="{{url('product_saller')}}">
         <img class="banner-category-img" src="{{('src/img/kitchen_icon/1.png')}}" alt="Image Alternative text" title="Image Title" />
         <h5 class="banner-category-title">Tawas
@@ -43,7 +40,7 @@
         </p>
       </a>
     </div>
-    <div class="col-md-2">
+    <div class="col-md-3">
       <a class="banner-category" href="{{url('product_saller')}}">
         <img class="banner-category-img" src="{{('src/img/kitchen_icon/3.png')}}" alt="Image Alternative text" title="Image Title" />
         <h5 class="banner-category-title">Pressure Cookers
@@ -52,7 +49,7 @@
         </p>
       </a>
     </div>
-    <div class="col-md-2">
+    <div class="col-md-3">
       <a class="banner-category" href="{{url('product_saller')}}">
         <img class="banner-category-img" src="{{('src/img/kitchen_icon/4.png')}}" alt="Image Alternative text" title="Image Title" />
         <h5 class="banner-category-title">Kitchen Tools
@@ -61,7 +58,7 @@
         </p>
       </a>
     </div>
-    <div class="col-md-2">
+    <div class="col-md-3">
       <a class="banner-category" href="{{url('product_saller')}}">
         <img class="banner-category-img" src="{{('src/img/kitchen_icon/5.png')}}" alt="Image Alternative text" title="Image Title" />
         <h5 class="banner-category-title">Gas Stoves
@@ -70,78 +67,12 @@
         </p>
       </a>
     </div>
-    <div class="col-md-2">
+    <div class="col-md-3">
       <a class="banner-category" href="{{url('product_saller')}}">
         <img class="banner-category-img" src="{{('src/img/kitchen_icon/6.png')}}" alt="Image Alternative text" title="Image Title" />
         <h5 class="banner-category-title">Rice Cookers
         </h5>
         <p class="banner-category-desc">437 products
-        </p>
-      </a>
-    </div>
-  </div>
-</div>
-<div class="container">
-  <h4 >Kitchen Storage
-  </h4>
-  <hr>
-  <div class="row row-sm-gap" data-gutter="15">
-    <div class="col-md-2">
-      <a class="banner-category" href="{{url('product_saller')}}">
-        <img class="banner-category-img" src="{{('src/img/kitchen_icon/7.png')}}" alt="Image Alternative text" title="Image Title" />
-        <h5 class="banner-category-title">Water Bottles
-        </h5>
-        <p class="banner-category-desc">173 products
-        </p>
-      </a>
-    </div>
-    <div class="col-md-2">
-      <a class="banner-category" href="{{url('product_saller')}}">
-        <img class="banner-category-img" src="{{('src/img/kitchen_icon/8.png')}}" alt="Image Alternative text" title="Image Title" />
-        <h5 class="banner-category-title">Lunch Box
-        </h5>
-        <p class="banner-category-desc">599 products
-        </p>
-      </a>
-    </div>
-  </div>
-</div>
-<div class="container">
-  <h4 >Tableware & Dinnerware
-  </h4>
-  <hr>
-  <div class="row row-sm-gap" data-gutter="15">
-    <div class="col-md-2">
-      <a class="banner-category" href="{{url('product_saller')}}">
-        <img class="banner-category-img" src="{{('src/img/kitchen_icon/9.png')}}" alt="Image Alternative text" title="Image Title" />
-        <h5 class="banner-category-title">Coffee Mug
-        </h5>
-        <p class="banner-category-desc">173 products
-        </p>
-      </a>
-    </div>
-    <div class="col-md-2">
-      <a class="banner-category" href="{{url('product_saller')}}">
-        <img class="banner-category-img" src="{{('src/img/kitchen_icon/10.png')}}" alt="Image Alternative text" title="Image Title" />
-        <h5 class="banner-category-title">Dinner Set
-        </h5>
-        <p class="banner-category-desc">599 products
-        </p>
-      </a>
-    </div>
-  </div>
-</div>
-<div class="container">
-  <h4 >Cleaning Supplies
-  </h4>
-  <hr>
-  <div class="row row-sm-gap" data-gutter="15">
-    <div class="col-md-2">
-      <a class="banner-category" href="{{url('product_saller')}}">
-        <img class="banner-category-img" src="{{('src/img/kitchen_icon/11.png')}}" alt="Image Alternative text" title="Image Title" />
-        <h5 class="banner-category-title">Cleaner
-        </h5>
-        <p class="banner-category-desc">173 products
         </p>
       </a>
     </div>
