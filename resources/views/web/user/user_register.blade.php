3e257582ff675430c2b1d26dcc5eb97e59db8605
gulzarc5 <gulzarc5@gmail.com>
2019-08-27 15:49:26 +0530

    user account Working

--- resources/views/web/user/user_register.blade.php
+++ resources/views/web/user/user_register.blade.php
@@ -7,39 +7,68 @@
         <h3 class="widget-title text-center">Create your Account
         </h3>
         <hr />
-        <form>
+        {{ Form::open(array('route' => 'web.user_registration', 'method' => 'post')) }}
           <div class="form-group">
-            <label>Email
-            </label>
-            <input class="form-control" type="text" />
+              <label>Name</label>
+              <input class="form-control" type="text" name="name" />
+                
+                @if($errors->has('name'))
+                    <span class="invalid-feedback" role="alert" style="color:red">
+                        <strong>{{ $errors->first('name') }}</strong>
+                    </span>
+                @enderror
           </div>
           <div class="form-group">
-            <label>Password
-            </label>
-            <input class="form-control" type="text" />
+            <label>Email</label>
+            <input class="form-control" type="email" name="email" />
+
+              @if($errors->has('email'))
+                  <span class="invalid-feedback" role="alert" style="color:red">
+                      <strong>{{ $errors->first('email') }}</strong>
+                  </span>
+              @enderror
           </div>
           <div class="form-group">
-            <label>Repeat Password
-            </label>
-            <input class="form-control" type="text" />
+            <label>Password</label>
+            <input class="form-control" type="password" name="password" />
+              @if($errors->has('password'))
+                  <span class="invalid-feedback" role="alert" style="color:red">
+                      <strong>{{ $errors->first('password') }}</strong>
+                  </span>
+              @enderror
           </div>
           <div class="form-group">
-            <label>Phone Number
-            </label>
-            <input class="form-control" type="text" />
+            <label>Repeat Password</label>
+            <input class="form-control" type="password" name="confirm-password_confirmation" />
+
+              @if($errors->has('password_confirmation'))
+                  <span class="invalid-feedback" role="alert" style="color:red">
+                      <strong>{{ $errors->first('password_confirmation') }}</strong>
+                  </span>
+              @enderror
           </div>
-          <div class="checkbox">
+          <div class="form-group">
+            <label>Phone Number</label>
+            <input class="form-control" type="number" name="mobile" />
+
+              @if($errors->has('mobile'))
+                  <span class="invalid-feedback" role="alert" style="color:red">
+                      <strong>{{ $errors->first('mobile') }}</strong>
+                  </span>
+              @enderror
+          </div>
+         {{--  <div class="checkbox">
             <label>
               <input class="i-check" type="checkbox" />Subscribe to the Newsletter
             </label>
-          </div>
+          </div> --}}
           <input class="btn btn-primary" type="submit" value="Create Account" />
-        </form>
+        {{ Form::close() }}
         <div class="gap gap-small">
         </div>
         <ul class="list-inline">
           <li>
-            <a href="#nav-login-dialog" class="popup-text">Already Memeber
+            <a href="#" class="popup-text">Already Memeber
             </a>
           </li>
         </ul>
