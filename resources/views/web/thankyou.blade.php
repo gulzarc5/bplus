9d85183d8f38d19c1e99f6f981f2dd1258de4df2
chimdevi <chinmayeedevi99@gmail.com>
2019-09-07 15:03:42 +0530

    thank you

--- resources/views/web/thankyou.blade.php
+++ resources/views/web/thankyou.blade.php
@@ -6,6 +6,9 @@
 
 @section('content')
 	<center>
-		<img src="{{asset('src/img/thankyou.png')}}">
+		<div style="margin-top: 10%; margin-bottom: 4%;">
+			<h2 >Thank You for Shopping</h2>
+			<p><a href="">continue to Shopping</a></p>
+		</div>
 	</center>
 @endsection
